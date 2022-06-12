7581b69e198f95145144e72b3d5896a2bbe0a7b4
root <root@kali>
2022-06-12 15:31:26 +0530

    Test class first assert

--- HelloJunit/test/com/suraj/course/junit/impl/GreetingsImplTest.java
+++ HelloJunit/test/com/suraj/course/junit/impl/GreetingsImplTest.java
@@ -1,14 +1,23 @@
 package com.suraj.course.junit.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
+
+import com.suraj.course.junit.interfaces.Greetings;
 
 class GreetingsImplTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void sayHelloProperly() {  //Test for sayHello
+//		fail("Not yet implemented");   //this is to fail
+		Greetings greeting = new GreetingsImpl();
+		String greet = greeting.sayHello("Abhi");
+		assertNotNull(greet);
+		assertEquals("My name is Abhi", greet);
+		//assertNotEquals("Abhi", greet);
 	}
 
 }
