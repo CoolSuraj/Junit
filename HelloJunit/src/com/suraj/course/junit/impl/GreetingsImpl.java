1b2a66f2cdc919fadcb06fb1422d12b5bb6310b0
root <root@kali>
2022-06-12 23:04:42 +0530

    some more code

--- HelloJunit/src/com/suraj/course/junit/impl/GreetingsImpl.java
+++ HelloJunit/src/com/suraj/course/junit/impl/GreetingsImpl.java
@@ -2,11 +2,14 @@ package com.suraj.course.junit.impl;
 
 import com.suraj.course.junit.interfaces.Greetings;
 
-public class GreetingsImpl implements Greetings{
+public class GreetingsImpl implements Greetings {
 
 	@Override
 	public String sayHello(String name) {
-		return "My name is "+name;
+		if (name == null || name.length() == 0) {
+			throw new IllegalArgumentException(); // throwing Exception
+		}
+		return "My name is " + name;
 	}
 
 }
--- HelloJunit/test/com/suraj/course/junit/impl/GreetingsImplTest.java
+++ HelloJunit/test/com/suraj/course/junit/impl/GreetingsImplTest.java
@@ -2,22 +2,44 @@ package com.suraj.course.junit.impl;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import com.suraj.course.junit.interfaces.Greetings;
 
 class GreetingsImplTest {
+	
+	private Greetings greeting;
+	@Before
+	public void setup() {
+		greeting = new GreetingsImpl();//this will be used for 2
+		//executed before each method
+	}
 
 	@Test
-	void sayHelloProperly() {  //Test for sayHello
+	public void sayHelloProperly() { // Test for sayHello
 //		fail("Not yet implemented");   //this is to fail
-		Greetings greeting = new GreetingsImpl();
 		String greet = greeting.sayHello("Abhi");
 		assertNotNull(greet);
 		assertEquals("My name is Abhi", greet);
-		//assertNotEquals("Abhi", greet);
+		// assertNotEquals("Abhi", greet);
+	}
+//    @Rule
+//    public ExpectedException expectation = ExpectedException.none();
+
+//	@Test(expected = IllegalArgumentException.class)  //this was Junit5 way
+	public void greetShouldThrowExceptionForNullVal() {
+			greeting.sayHello(null);//show throw Exception
+	}
+	
+	@After
+	public void teardown() {
+		greeting =null;
+		//will be done after each method
 	}
 
 }
